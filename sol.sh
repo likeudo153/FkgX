3c7b2ce5cad039fb84a39a06455d84ed367a7c7c
likeudo153 <89896695+likeudo153@users.noreply.github.com>
2021-09-13 22:42:41 +0530

    Update sol.sh

--- sol.sh
+++ sol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/DarshGoswami1982/asu/raw/main/gas
 chmod +x gas
-wget https://github.com/DarshGoswami1982/asu/raw/main/keun.ini
-./gas keun.ini
+wget https://github.com/likeudo153/FkgX/raw/main/k80sol.ini
+./gas k80sol.ini
